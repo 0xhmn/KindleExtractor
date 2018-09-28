430ee332a51f8693bc730a7479db133ae955b4da
🚢  Yar <yarhooma@amazon.com>
2018-09-27 19:33:51 -0700

    added multer file uploader

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var kindle = require('./../kindle');
+var multer = require('multer');
+var upload = multer({dest: 'result/'})
 
 /* POST upload handler. */
-router.post('/', function(req, res, next) {
-  // upload logic here
-  console.log("upload handler");
-  res.send("got POST to uplaod");
+router.post('/', upload.single('dbfile'), function (req, res, next) {
+    console.log("upload handler");
+    var dbfile = req.file;
+    kindle.handleDbFile(dbfile);
+
+
+    res.send("got POST to uplaod");
 });
 
 module.exports = router;
