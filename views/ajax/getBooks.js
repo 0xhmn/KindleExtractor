9e7722ff18b6050ae135d177d58279b1ed97f458
🚢  Yar <yarhooma@amazon.com>
2018-10-02 16:38:49 -0700

    Added Ajax handler to get all books

---
+++ views/ajax/getBooks.js
@@ -0,0 +1,24 @@
+$("form#idFormUpload").submit(function (e) {
+
+    // disable the default action
+    e.preventDefault();
+
+    var formData = new FormData(this);
+
+    $.ajax({
+        type: 'POST',
+        url: '/get-list',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            console.log('Submission was successful.');
+            console.log(data);
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            console.log(data);
+        },
+    });
+});
